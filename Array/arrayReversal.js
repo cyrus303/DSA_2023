451bda704ad46ef22177205d8634105e521dc1cb
cyrus303 <sachinsunny303@gmail.com>
2023-01-30 19:52:57 +0000

    Array reversal program using reverse loop

--- Array/arrayReversal.js
+++ Array/arrayReversal.js
@@ -1,1 +1,47 @@
-//helllo
+/*
+Write a program to reverse an array or string
+
+Given an array (or string), the task is to reverse the array/string.
+
+Examples : 
+
+Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
+*/
+function arrayReversal(arr) {
+  arr2 = [];
+  count = 0;
+  for (let index = arr.length - 1; index >= 0; index--) {
+    const element = arr[index];
+    if (count < arr.length) {
+      arr2[count] = element;
+      count++;
+    }
+  }
+  console.log(`input array: ${arr}
+  output array: ${arr2}`);
+}
+
+// arrayReversal([1, 2, 3, 4, 5]);
+
+function arrrayReversal2(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = Math.ceil(arr.length / 2);
+  let temp;
+
+  console.log(start, end, mid);
+
+  while (start < mid) {
+    temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+  }
+  console.log(arr);
+}
+
+arrrayReversal2([1, 2, 3, 4, 5]);
