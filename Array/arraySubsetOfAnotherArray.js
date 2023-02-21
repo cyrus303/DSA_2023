62e131bed02dd1f6eb3cfb085b9df252f6ae7810
cyrus303 <sachinsunny303@gmail.com>
2023-02-21 07:16:08 +0000

    array subset problem

--- Array/arraySubsetOfAnotherArray.js
+++ Array/arraySubsetOfAnotherArray.js
@@ -29,6 +29,7 @@ Expected Auxiliary Space: O(n)
 
 function checkSubset(Array1, Array2) {
   let Array1_Counter = {};
+  let Array2_Counter = {};
   let subset = true;
 
   Array1.forEach((element) => {
@@ -36,31 +37,24 @@ function checkSubset(Array1, Array2) {
   });
 
   Array2.forEach((element) => {
-    if (!(element in Array1_Counter)) {
+    Array2_Counter[element] = (Array2_Counter[element] || 0) + 1;
+  });
+
+  console.log(Array1_Counter);
+  console.log(Array2_Counter);
+
+  Object.keys(Array2_Counter).forEach((element) => {
+    if (Array2_Counter[element] <= Array1_Counter[element] && subset === true) {
+      subset = true;
+    } else {
       subset = false;
     }
   });
-
   return subset;
 }
 
-let Array1 = [11, 1, 13, 21, 3, 75];
-let Array2 = [11, 3, 75, 1];
+let Array1 = [1, 2, 3, 4, 5];
+let Array2 = [1, 2, 3, 1, 1];
 
 const value = checkSubset(Array1, Array2);
 console.log(value);
-
-/*
-
-fails for multiple element in array 1
-
-For Input: 
-8 4
-1 2 3 4 5 6 7 8
-1 2 3 1
-Your Code's output is: 
-Yes
-It's Correct output is: 
-No
-
-*/
