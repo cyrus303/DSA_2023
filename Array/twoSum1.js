d0c79d60b2646e9fcb6208a48cf1c44262b1f530
cyrus303 <sachinsunny303@gmail.com>
2023-03-01 07:34:25 +0000

    two sum-2 for sorted array

--- Array/twoSum1.js
+++ Array/twoSum1.js
@@ -70,4 +70,40 @@ let nums2 = [1, 5, 7, -1, 5];
 let n = nums2.length;
 let k = 6;
 
-countPairs(nums2, n, k);
+// countPairs(nums2, n, k);
+
+/*
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly 
+one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+*/
+
+var twoSum = function (arr, k) {
+  let counterHash = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    let diff = k - arr[i];
+
+    if (counterHash.has(diff)) {
+      return [counterHash.get(diff) + 1, i + 1];
+    }
+
+    counterHash.set(arr[i], i);
+  }
+
+  console.log(counterHash);
+};
+
+twoSum(nums2, k);
